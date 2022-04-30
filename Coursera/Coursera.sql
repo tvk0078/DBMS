462297772951ad832ce2cb56461fc5e377ebc12b
Vishal <tvk0078@gmail.com>
2022-04-30 18:13:44 +0530

    Coursera Assignments

---
+++ Coursera/Coursera.sql
@@ -0,0 +1,44 @@
+//problem-1
+table - Student
+primary key- StdNo
+other columns-StdAddress, StdCity, StdState, StdZip,StdEmail
+     
+table- Lender
+Primary Key- LenderNo
+other Column-LendName
+     
+table -InstID
+Primary Key-InstNamehttps://github.com/tvk0078/DBMS/blob/main/SQL_FILES.sql
+Other Column-InstMascot
+     
+table -Loan
+Primary Key-LoanNo
+Other Column-ProdDate, DisbMethod,DisbBank, DateAuth, NoteValue, Subsidized, Rate
+    
+table -DisBurseLine
+Primary Key-DateSent
+Other Column-Amount,OrigFee,GuarFee
+     
+     
+//problem-2     
+1-M relationship-->Uses,Authorizes,Sent
+
+//problem-3        
+M-N relationship-->NOne
+         
+//Problem-4
+Identifying Relationship-->Sent
+        
+//problem-5
+table-Account
+Primary key- Acctid
+Other column -AcctName, Balance
+       
+//problem-6
+Owner-owner
+Primary key- OwnId
+other column- OwnName, OwnPhone
+       
+Table-Property 
+Primary Key - PropId
+Other Column- BidgNmae,UnitNo, Bdrms    
